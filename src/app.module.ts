bd07c8a60ac96afb3a01345a33a6057c409f553b
ehoorxy <ehoorxy@gmail.com>
2024-04-30 00:51:04 -0300

    Adding .env configuration for database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilmsModule } from './films/films.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, FilmsModule],
+  imports: [
+    UsersModule,
+    FilmsModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_DIALECT as 'postgres',
+      host: process.env.DB_HOST as string,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
